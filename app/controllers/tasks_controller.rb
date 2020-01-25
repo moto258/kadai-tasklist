031262988f56e1696c4b7cca4a2333d9bcbfa8a5
moto.yama <ymmt369@gmail.com>
2020-01-25 21:29:57 +0000

    add user signup login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,12 @@
 class TasksController < ApplicationController
+    before_action :require_user_logged_in, only: [:index, :show]
     before_action :set_task, only: [:show, :edit, :update, :destroy]
     
     def index
-        @tasks = Task.all
+        if logged_in?
+            @task = current_user.tasks.build
+            @tasks =current_user.tasks.order(id: :desc).page(params[:page])
+        end
     end
     
     def show
@@ -13,12 +17,12 @@ class TasksController < ApplicationController
     end
     
     def create
-        @task = Task.new(task_params)
-        
+        @task = current_user.tasks.build(task_params)
         if @task.save
             flash[:success] = 'Task が正常に作成されました'
-            redirect_to @task
+            redirect_to root_url
         else
+            @tasks = current_user.tasks.order(id: :desc).page(params[:page])
             flash.now[:danger] = 'Task が作成されませんでした'
             render :new
         end
